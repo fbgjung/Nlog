cb8dc01233af0b8c436470b55a7e2b7608d6e1f8
fbgjung <fbgjung@gmail.com>
2025-04-24 14:55:03 +0900

    feat: GoogleAnalytics 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Header } from "@/layouts/Header";
 import { Footer } from "@/layouts/Footer";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "류금정 개발 블로그",
@@ -19,6 +20,9 @@ export default function RootLayout({
         <Header />
         <main className="mt-[64px] flex-1">{children}</main>
         <Footer />
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+        )}
       </body>
     </html>
   );
